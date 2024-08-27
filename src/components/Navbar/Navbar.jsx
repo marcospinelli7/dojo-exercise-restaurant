8cfa2146950ab36b3bdc07526dbfc24e1b0b82ac
marcospinelli7 <marco.spinelli@beije.it>
2024-08-27 16:57:52 +0200

    Create Navbar Component + Stylesheet

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+
+  return (
+    <div className='navbar-container'>
+      <div className="navbar-title">
+        <img src="./assets/icons/logo.svg" alt="nav_img"/>
+        <h1>Restoran</h1>
+      </div>
+      <div className="navbar-links">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
