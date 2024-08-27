a61a2de27682570ac07267b8e521fa5adf22f7cf
marcospinelli7 <marco.spinelli@beije.it>
2024-08-27 17:01:54 +0200

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+//components
+import { PageLayout } from "./components";
 
 
 function App() {
   return (
     <div className="App">
-
+      <PageLayout>
+        <h1>Homepage</h1>
+      </PageLayout>
     </div>
   );
 }
