20441842d795cb5b9f7d8b90a6dcefbef0853b1b
marcospinelli7 <marco.spinelli@beije.it>
2024-09-13 17:40:14 +0200

    Update Service with the dedicated component + StyleSheet

--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,14 +1,58 @@
 import React from 'react'
-import { HeroSection } from '../../components'
+import { HeroSection, ServiceCard } from '../../components'
+import './ServicePage.css'
 
 const ServicePage = () => {
   return (
     <>
       <HeroSection 
-          pageTitle="Service"
+          pageTitle="Services"
           pagePath="service"
       />
+      <div className="service-section responsive-padding">
+        <ServiceCard 
+          icon="/logo.svg"
+          title="Master Chefs"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />
+        <ServiceCard 
+          icon="/logo.svg"
+          title="Quality Food"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />
+        <ServiceCard 
+          icon="/logo.svg"
+          title="Online Order"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />
+        <ServiceCard 
+          icon="/logo.svg"
+          title="24/7 Service"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />     
+                <ServiceCard 
+          icon="/logo.svg"
+          title="Master Chefs"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />
+        <ServiceCard 
+          icon="/logo.svg"
+          title="Quality Food"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />
+        <ServiceCard 
+          icon="/logo.svg"
+          title="Online Order"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />
+        <ServiceCard 
+          icon="/logo.svg"
+          title="24/7 Service"
+          description="Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam"
+        />   
+      </div>
     </> 
+    
   )
 }
 
