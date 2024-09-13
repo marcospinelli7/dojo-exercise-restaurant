acd6ff67c743fce92934e2817094583a8f631921
marcospinelli7 <marco.spinelli@beije.it>
2024-09-13 17:50:46 +0200

    Update Menu Component, add API Data render and Stylesheet updates

--- src/components/MenuSection/MenuSection.jsx
+++ src/components/MenuSection/MenuSection.jsx
@@ -3,24 +3,60 @@ import './MenuSection.css'
 
 
 const MenuSection = () => {
-  const [ menuType, setMenuType ] = useState(1);
+    const [ menuType, setMenuType ] = useState(1);
+    const [ apiData, setApiData ] = useState({
+        pizzas: [],
+        drinks: [],
+        lunch: []
+    });
+
+    useEffect(() => {
+        fetch("https://private-anon-5f0f608723-pizzaapp.apiary-mock.com/restaurants/restaurantId/menu?category=Pizza&orderBy=rank", {
+        method: "GET"
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            const pizzas = data.filter((item) => {
+                if (item.category === "Pizza") {
+                    return item
+                }
+            })
+            const drinks = data.filter((item) => {
+                if (item.category === "Dryck") {
+                    return item
+                }
+            })
+            const lunch = data.filter((item) => {
+                if (item.category === "Tillbehör") {
+                    return item
+                }
+            })
+            setApiData({ pizzas: pizzas, drinks: drinks, lunch: lunch})
+        })
+        .catch((error) => console.log(error));
+    }, [])
 
   const toogleMenuType = (n) => {
-    setMenuType(n)
+    setMenuType(n);
   }
-  
+
   const toogleActive = (e) => {
     const currTarget = e.currentTarget;   
+    console.log(currTarget);
     document.querySelectorAll('.selector').forEach( btn => 
     btn.classList.remove('active'))
-    e.classList.add('active');
+    currTarget.classList.add('active')
   }
 
   return (
-    <div className='menu-section-container'>
+    <div className='menu-section-container responsive-padding'>
        <div className="menu-section-selector">
         <div className="selector active" 
-        onClick={() => {
+        onClick={(e) => {
+            document.querySelectorAll('.selector').forEach( btn => 
+            btn.classList.remove('active'))
+            e.currentTarget.classList.add("active")
             toogleMenuType(1);
         }}>
             <img src="./assets/icons/logo.svg" alt="selector_icon" />
@@ -31,7 +67,10 @@ const MenuSection = () => {
             </p>
         </div>
         <div className="selector" 
-        onClick={() => {
+        onClick={(e) => {
+            document.querySelectorAll('.selector').forEach( btn => 
+            btn.classList.remove('active'))
+            e.currentTarget.classList.add("active")
             toogleMenuType(2);
         }}
         >
@@ -43,7 +82,10 @@ const MenuSection = () => {
             </p>
         </div>
         <div className="selector" 
-        onClick={() => {
+        onClick={(e) => {
+            document.querySelectorAll('.selector').forEach( btn => 
+            btn.classList.remove('active'))
+            e.currentTarget.classList.add("active")
             toogleMenuType(3);
         }}>
             <img src="./assets/icons/logo.svg" alt="selector_icon" />
@@ -56,19 +98,49 @@ const MenuSection = () => {
        </div>
        <div className="menu-items">
             {menuType === 1 ? (
-                <div className="item">
-                    First section
-                </div>
-            ) : ""}
+                apiData.pizzas.map((item) => {
+                    return (
+                        <div className="item" key={item.id}>
+                            <div>
+                                <p className='item-name'>{item.name}</p>
+                                <p className='item-price'>{item.price}€</p>
+                            </div>
+                            {item.topping ? ( 
+                                <p className='item-desc'>{item.topping.reduce((a, b) => { return a + ", " + b})}</p>
+                            ) : ""}
+                        </div>
+                    )
+                })
+            ) : "" }
             {menuType === 2 ? (
-                <div className="item">
-                    Second section
-                </div>
+                apiData.drinks.map((item) => {
+                    return (
+                        <div className="item" key={item.id}>
+                            <div>
+                                <p className='item-name'>{item.name}</p>
+                                <p className='item-price'>{item.price}€</p>
+                            </div>
+                            {item.topping ? ( 
+                                <p className='item-desc'>{item.topping.reduce((a, b) => { return a + ", " + b})}</p>
+                            ) : ""}
+                        </div>
+                    )
+                })            
             ) : ""}
             {menuType === 3 ? (
-                <div className="item">
-                    Third section
-                </div>
+                apiData.lunch.map((item) => {
+                    return (
+                        <div className="item" key={item.id}>
+                            <div>
+                                <p className='item-name'>{item.name}</p>
+                                <p className='item-price'>{item.price}€</p>
+                            </div>
+                            {item.topping ? ( 
+                                <p className='item-desc'>{item.topping.reduce((a, b) => { return a + ", " + b})}</p>
+                            ) : ""}
+                        </div>
+                    )
+                })
             ) : ""}
        </div>
     </div>
